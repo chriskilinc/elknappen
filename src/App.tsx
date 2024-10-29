ff2f3498ad599cb932b6b5c92d7d20c894bac9ff
Christopher Kilinc <chriskilinc@gmail.com>
2024-10-29 23:58:12 +0100

    Header WIP

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,34 @@ function App() {
 
   return (
     <>
-      <h1>Elknappen</h1>
+      <header className="header">
+        <div className='container'>
+          <div>
+            <p>logo</p>
+            <h1>El Knappen AB</h1>
+          </div>
+          <nav>
+            <ul>
+              <li>Start</li>
+              <li>Tjänster</li>
+              <li>Företaget</li>
+              <li>Kontakt</li>
+            </ul>
+          </nav>
+
+        </div>
+
+      </header>
+      <main className="main">
+        <p>main</p>
+      </main>
+      <footer className="footer">
+        <p>footer</p>
+      </footer>
     </>
   )
 }
 
 export default App
+
+//  http://web.archive.org/web/20220113181653/https://www.elknappen.se/
